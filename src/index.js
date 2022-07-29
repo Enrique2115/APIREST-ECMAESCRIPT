f8fb65ea9359a10b9cee2b9ca7a5cc59eb080060
Luis2115 <lmorochofebres@gmail.com>
2022-07-29 00:37:40 -0500

    initial roles and project update

--- src/index.js
+++ src/index.js
@@ -4,20 +4,44 @@ import { PORT } from "./config";
 import { handleError } from "./libs/handleErrors";
 import { sequelize } from "./db/mysql";
 
-import "./models/Login";
+import "./models/user.model";
+import "./models/role.model";
 
 // //Descomentar cuando se use MongoDB
 // import "./db/mongodb";
 
 //imports rutas personalizadas
-import generico from "./routes/generic.routes";
+import generico from "./routes/v1/generic.routes";
+import { Role } from "./models/role.model";
+
+/**
+ * Create three roles in the database, if they don't already exist.
+ */
+function initialRoles() {
+  Role.create({
+    id_role: 1,
+    name: "admin",
+  });
+
+  Role.create({
+    id_role: 2,
+    name: "employee",
+  });
+
+  Role.create({
+    id_role: 3,
+    name: "guest",
+  });
+}
 
 async function main() {
   try {
-    await sequelize.sync();
+    await sequelize.sync().then(() => {
+      // initialRoles();
+    });
 
     //---- Routes Personalizadas ----//
-    app.use("/api", generico);
+    app.use("/api/v1/auth", generico);
 
     app.use(handleError); //---- Manejo de errores ----//
 
