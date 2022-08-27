af98334dbd6328c9de083703a64f46480dc970bc
Luis2115 <lmorochofebres@gmail.com>
2022-08-27 13:10:10 -0500

    function handleSendEmail

---
+++ src/libs/handleSendEmail.js
@@ -0,0 +1,33 @@
+import nodemailer from "nodemailer";
+import { MAILTRAP, URL_FRONTEND } from "../config";
+import { templateEmail } from "../views/templateEmail";
+
+export function sendEmail(email, token) {
+  const mail = nodemailer.createTransport({
+    host: MAILTRAP.MAIL_HOST,
+    port: MAILTRAP.MAIL_PORT,
+    auth: {
+      user: MAILTRAP.MAIL_USER,
+      pass: MAILTRAP.MAIL_PASS,
+    },
+  });
+
+  const url = `${URL_FRONTEND}/recovery-password?token=${token}`;
+
+  const mailOptions = {
+    from: "Dashboard Server <support_dashboard@gmail.com>",
+    to: email,
+    subject: "Enlace para restablecer la contraseña - Dashboard",
+    html: templateEmail(url),
+  };
+
+  mail.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(1);
+      return 1;
+    } else {
+      console.log(0);
+      return 0;
+    }
+  });
+}
